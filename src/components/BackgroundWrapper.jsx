07f9f6100770871542f7977b308eded7d0607e70
thiago88caires <thiago88caires@gmail.com>
2025-08-10 02:00:15 -0300

    Refactor code structure for improved readability and maintainability

--- src/components/BackgroundWrapper.jsx
+++ src/components/BackgroundWrapper.jsx
@@ -4,11 +4,11 @@ function BackgroundWrapper({ children }) {
             {/* Imagem de fundo */}
             <div
                 className="absolute inset-0 bg-cover bg-no-repeat bg-center z-0"
-                style={{ backgroundImage: "url('/src/assets/img/background.png')" }}
+                style={{ backgroundImage: "url('/src/assets/img/background_new.png')" }}
             ></div>
 
             {/* Camada de gradiente sobre a imagem */}
-            <div className="absolute inset-0 bg-gradient-to-b from-white/10 via-[#dbeeff]/30 to-white/40 z-10"></div>
+            <div className="absolute inset-0 bg-gradient-to-b from-white/20 via-blue-50/40 to-white/60 z-10"></div>
 
             {/* Conteúdo sobreposto */}
             <div className="relative z-20">
