78dd5d57aeaee3e51dbfe42333c9d0f885082b4a
Isabela Camara <belacstar@gmail.com>
2025-08-30 19:50:29 -0300

    Removendo backend

--- casa-alegria-backend/src/routes/reserva.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { criarReserva } = require('../controllers/reserva.controller');
-
-// Rota POST para criação de reserva
-router.post('/', criarReserva);
-
-module.exports = router;
